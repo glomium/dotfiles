5dd06be745e3b6bf0230fea9f56f087f5ce7f7d3
Sebastian Braun <sebastian@elmnt.de>
2024-09-14 10:41:25 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ DIR=$(pwd)
 
 ln -s $DIR/files/vimrc ~/.vimrc
 
+# On devcontainer install common utils
+# https://github.com/devcontainers/features/blob/main/src/common-utils/main.sh
 echo "TEST" > ~/setup-test
