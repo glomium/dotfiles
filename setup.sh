f7df9a91359d69d41d5384055c27c42f9aa88514
Sebastian Braun <sebastian@elmnt.de>
2024-09-15 05:17:08 +0200

    update

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 DIR=$(pwd)
 
-ln -s $DIR/files/vimrc ~/.vimrc
+ln -fs $DIR/files/vimrc ~/.vimrc
+
+if [ -f /run/.containerenv ] || [ -f /.dockerenv ]; then
+    echo "In Script runs in Container"
+fi
 
 # On devcontainer install common utils
 # https://github.com/devcontainers/features/blob/main/src/common-utils/main.sh
 echo "TEST" > ~/setup-test
+
